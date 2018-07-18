3ff5321ff71c70bd65c9cc497184f58b94b25439
AliakseiN <public.nechpai@gmail.com>
2018-07-18 21:19:12 +0300

    static first commit

---
+++ StaticExample.java
@@ -0,0 +1,29 @@
+
+public class StaticExample {
+
+	
+	static int a=5;
+	int b = 4;
+	
+	 static void  printVar () {
+	
+	}
+	 void printVar1 () {
+		
+	}
+	
+	public void sample () {
+		
+		printVar();
+		printVar1();
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+}
