cc94e42832eb731bdac44300a3f2638ff02bd4d2
Aliaksei Nechpai <Aliaksei_Nechpai@epam.com>
2018-07-24 18:30:47 +0300

    upload initial version to master

---
+++ HT11/Code/Lesson11/src/Task6.java
@@ -0,0 +1,27 @@
+import java.util.Arrays;
+import java.util.List;
+import java.io.*;
+
+public class Task6 {
+	
+	public static final String HI_THERE = "Hi there, %s." ; //create final variable
+
+	public static void main(String[] args) {
+		
+		// В зависимости от признака (0 или 1) в каждой строке текста удалить указанный символ везде, где он встречается, или вставить его после K-го символа.
+		
+		
+		
+		
+		
+		
+		String  text = "Hello, if you are reading this then you did a great job";
+		
+		byte [] textToByte =text.getBytes();
+		System.out.println(textToByte.toString());
+		
+	
+		
+	}
+
+}
